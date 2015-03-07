705ed683a4dbb003cb57c1e6cd97e966501c903b
Adam Smith <adamsmith@youlum.com>
2015-03-07 04:40:11 -0800

    Added methods and logic to the App constructor

--- test/app-core/app.js
+++ test/app-core/app.js
@@ -21,10 +21,16 @@
      * @private
      */
     var pass;
+    /**
+     * @type {Object}
+     * @private
+     */
+    var _this;
 
     // Check the user inputs
     vals = [ config, sources, categories, questions ];
     pass = App.checkType(vals, 'object');
+    _this = this;
 
     /**
      * ---------------------------------------------------
@@ -179,31 +185,298 @@
 
     /**
      * ---------------------------------------------------
+     * Public Property (App.searchBar)
+     * ---------------------------------------------------
+     * @type {SearchBar}
+     */
+    this.searchBar = ( (pass) ?
+      new SearchBar(this.sources, this.categories) : null
+    );
+
+    /**
+     * ---------------------------------------------------
      * Public Property (App.questions)
      * ---------------------------------------------------
      * @type {Questions}
      */
     this.questions = (pass) ? new Questions(questions) : null;
 
-    /**
-     * ---------------------------------------------------
-     * Private Property (App.questions)
-     * ---------------------------------------------------
-     * @type {Questions}
-     */
-    this.questions = (pass) ? new Questions(questions) : null;
-
+    // Initialize the app
     if (pass) {
-      
+
+      // Update the config and search bar
+      this.config.setSearchDefaults(config.searchDefaults, this.searchBar,
+                                    this.questions.length);
+      this.searchBar.updateVals(this.config.searchBar.defaults);
+
+      if (this.config.worker) {
+        // --> WebWorker.init();
+      }
+
+      document.addEventListener('DOMContentLoaded', function() {
+        // --> setScrollbar();
+        // --> appendMain();
+        // --> setPrettyCode();
+        // --> appendNav();
+        // --> DisplaySearchBar.init();
+        // --> AddEvents.init();
+        if (!_this.config.worker) {
+          // --> FormatQuestions.init();
+          // --> AppendQuestions.init();
+          // --> DisplayQuestions.assembleQuestions();
+        }
+        else {
+          _this.flags.initDone = true;
+          if (_this.flags.workerFail) {
+            getClass('loadError')[0].style.display = 'block';
+          }
+          if (_this.flags.workerPass || _this.flags.workerFail) {
+            // --> DisplayQuestions.assembleQuestions();
+          }
+        }
+        DEBUG && debug.group('init', 'end');
+      });
     }
     else {
       // Show error message
       document.addEventListener('DOMContentLoaded', function() {
-        appendMain();
-        appendError();
+        // --> appendMain();
+        // --> appendError();
         DEBUG && debug.group('init', 'end');
       });
     }
+/******** CONVERT FOLLOWING PRIVATE METHODS TO FIT NEW FORMAT ****/
+    /**
+     * ---------------------------------------------
+     * Private Method (setScrollbar)
+     * ---------------------------------------------
+     * saves the width of the browser's scrollbar
+     * @type {function()}
+     * @private
+     */
+    function setScrollbar() {
+      // Debugger
+      DEBUG.LoadApp.call && console.log(
+        'CALL: LoadApp.setScrollbar()'
+      );
+      // Declare method variables
+      var div;
+      // Create temporary div
+      div = document.createElement('div');
+      div.className = 'aIV-scrollbar';
+      // Add div to body
+      document.body.appendChild(div);
+      // Save the scrollbar width
+      configuration.scrollbar = div.offsetWidth - div.clientWidth;
+      // Remove div 
+      document.body.removeChild(div);
+    }
+
+    /**
+     * ---------------------------------------------
+     * Private Method (setPrettyCode)
+     * ---------------------------------------------
+     * set the height and padding options for syntax
+     *  highlighting
+     * @type {function()}
+     * @private
+     */
+    function setPrettyCode() {
+      // Debugger
+      DEBUG.LoadApp.call && console.log(
+        'CALL: LoadApp.setPrettyCode()'
+      );
+      // Declare method variables
+      var pre, code, ol, li, liHeight, olHeight;
+      // Create temporary elements
+      pre  = document.createElement('pre');
+      code = document.createElement('code');
+      ol   = document.createElement('ol');
+      li   = document.createElement('li');
+      // Hide from user view
+      pre.style.opacity = '0';
+      // Add dummy content
+      li.textContent = 'test';
+      // Append elements
+      roots.root.appendChild(pre);
+      pre.appendChild(code);
+      code.appendChild(ol);
+      ol.appendChild(li);
+      // Save line and list values
+      liHeight = li.offsetHeight;
+      olHeight = ol.offsetHeight - liHeight;
+      // Debugger
+      DEBUG.LoadApp.state && console.log(
+        'STATE: LoadApp.setPrettyCode() ' +
+        'Note: liHeight= %d, olHeight= %d', liHeight, olHeight
+      );
+      // Save the heights
+      configuration.prettyCode.olHeight = olHeight;
+      configuration.prettyCode.liHeight = liHeight;
+      // Remove elements
+      roots.root.removeChild(pre);
+    }
+
+    /**
+     * ---------------------------------------------
+     * Private Method (appendMain)
+     * ---------------------------------------------
+     * inserts the main html elements of this module
+     * @type {function()}
+     * @private
+     */
+    function appendMain() {
+      // Debugger
+      DEBUG.LoadApp.call && console.log(
+        'CALL: LoadApp.appendMain()'
+      );
+      // Declare method variables
+      var message, root, h1, sel, main,
+          nav, qs, error, h2, p, loader;
+      // Set error message
+      message = '' +
+      'The web worker failed. Please wait ' +
+      'while the app is being loaded manually.';
+      // Create tags
+      root   = document.createElement('div');
+      h1     = document.createElement('h1');
+      sel    = document.createElement('nav');
+      main   = document.createElement('div');
+      nav    = document.createElement('nav');
+      qs     = document.createElement('section');
+      error  = document.createElement('div');
+      h2     = document.createElement('h2');
+      p      = document.createElement('p');
+      loader = document.createElement('img');
+      // Add ids
+      root.id = 'aIV';
+      sel.id  = 'aIV-selections';
+      main.id = 'aIV-main';
+      nav.id  = 'aIV-nav';
+      qs.id   = 'aIV-questions';
+      // Add classes
+      sel.className    = 'selections';
+      main.className   = 'main';
+      qs.className     = 'questions';
+      error.className  = 'loadError';
+      loader.className = 'loader';
+      // Add content
+      h1.textContent = 'Algorithm IV';
+      h2.textContent = 'Load Error';
+      p.textContent  = message;
+      loader.src     = 'images/loading.gif';
+      // Append elements
+      document.body.appendChild(root);
+      root.appendChild(h1);
+      root.appendChild(sel);
+      root.appendChild(main);
+      main.appendChild(nav);
+      main.appendChild(qs);
+      qs.appendChild(error);
+      error.appendChild(h2);
+      error.appendChild(p);
+      qs.appendChild(loader);
+      // Save root element references
+      roots.root = root;
+      roots.sel  = sel;
+      roots.main = main;
+      roots.nav  = nav;
+      roots.qs   = qs;
+    }
+
+    /**
+     * ---------------------------------------------
+     * Private Method (appendNav)
+     * ---------------------------------------------
+     * inserts the main html elements of this module
+     * @type {function()}
+     * @private
+     */
+    function appendNav() {
+      // Debugger
+      DEBUG.LoadApp.call && console.log(
+        'CALL: LoadApp.appendNav()'
+      );
+      // Declare method variables
+      var prev, pArrow, pBG, pTitle, nav,
+          next, nArrow, nBG, nTitle;
+      // Create tags
+      prev   = document.createElement('div');
+      pArrow = document.createElement('div');
+      pBG    = document.createElement('div');
+      pTitle = document.createElement('div');
+      next   = document.createElement('div');
+      nArrow = document.createElement('div');
+      nBG    = document.createElement('div');
+      nTitle = document.createElement('div');
+      // Add ids
+      pArrow.id = 'aIV-prev';
+      nArrow.id = 'aIV-next';
+      // Add classes
+      prev.className = 'prev';
+      next.className = 'next';
+      pArrow.className = nArrow.className = 'arrow';
+      pBG.className    = nBG.className    = 'bg';
+      pTitle.className = nTitle.className = 'title';
+      // Add content
+      pTitle.textContent = pArrow.textContent = 'Previous';
+      nTitle.textContent = nArrow.textContent = 'Next';
+      // Set nav element
+      nav = getID('aIV-nav');
+      // Append elements
+      nav.appendChild(prev);
+      nav.appendChild(next);
+      prev.appendChild(pArrow);
+      prev.appendChild(pBG);
+      prev.appendChild(pTitle);
+      next.appendChild(nArrow);
+      next.appendChild(nBG);
+      next.appendChild(nTitle);
+    }
+
+    /**
+     * ---------------------------------------------
+     * Private Method (appendError)
+     * ---------------------------------------------
+     * insert a module call error html elements
+     * @type {function()}
+     * @private
+     */
+    function appendError() {
+      // Debugger
+      DEBUG.LoadApp.call && console.log(
+        'CALL: LoadApp.appendError()'
+      );
+      // Declare method variables
+      var message, error, h2, q;
+      // Set app error message
+      message = '' +
+      'Algorithm IV\'s initialization was triggered '   +
+      'with an incorrect argument. Please do not edit ' +
+      'the initialization function located at the '     +
+      'bottom of algorithmIVData.js. If this error '    +
+      'persists please <a href="https://github.com'     +
+      '/imaginate/algorithmiv/issues" class="dark">'    +
+      'open an issue</a> on the Algorithm IV GitHub '   +
+      'repo. I hope Algorithm IV is able to help you '  +
+      'learn more!&NewLine;- Adam';
+      // Create tags
+      error = document.createElement('div');
+      h2    = document.createElement('h2');
+      p     = document.createElement('p');
+      // Add classes
+      error.className = 'initError';
+      // Add content
+      h2.textContent = 'Setup Error';
+      p.textContent = message;
+      // Append elements
+      roots.qs.appendChild(error);
+      error.appendChild(h2);
+      error.appendChild(p);
+      // Hide loader
+      getClass('loader')[0].style.display = 'none';
+    }
+/******** END OF PRIVATE METHODS TO CONVERT ****/
   };
 
   /**
