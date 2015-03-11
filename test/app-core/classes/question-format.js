a0e4e4e12d60996f35635a98aed03649677ae641
Adam Smith <adamsmith@youlum.com>
2015-03-10 20:51:58 -0700

    Updating the QuestionFormat class

--- test/app-core/classes/question-format.js
+++ test/app-core/classes/question-format.js
@@ -4,10 +4,15 @@
    * -----------------------------------------------------
    * @desc An object containing the formatted details of a question.
    * @param {Object} question - The pre-formatted details of the question.
-   * @param {number=} index - This question's index (used solely for debug).
    * @constructor
    */
-  var QuestionFormat = function(question, index) {
+  var QuestionFormat = function(question) {
+
+    /**
+     * @type {PrettifiedList}
+     * @private
+     */
+    var code;
 
     /**
      * ---------------------------------------------------
@@ -18,9 +23,9 @@
     this.debug = (DEBUG) ? new Debug('QuestionFormat') : null;
 
     if (DEBUG) {
-      this.debug.group('init', 'coll', 'question', question, 'id', ++index);
-      this.debug.start('init', question, index);
-      this.debug.args('init', question, 'object', index, 'number=');
+      this.debug.group('init', 'coll', 'id', question.id, 'question', question);
+      this.debug.start('init', question);
+      this.debug.args('init', question, 'object');
     }
 
     /**
@@ -28,45 +33,27 @@
      * Public Property (QuestionFormat.id)
      * -----------------------------------------------
      * @desc The id for this question.
-     * @type {{
-     *   flag   : boolean,
-     *   content: string
-     * }}
+     * @type {string}
      */
-    this.id = {
-      flag   : false,
-      content: ''
-    };
+    this.id = '';
 
     /**
      * ----------------------------------------------- 
      * Public Property (QuestionFormat.source)
      * -----------------------------------------------
      * @desc This question's source.
-     * @type {{
-     *   flag   : boolean,
-     *   content: string
-     * }}
+     * @type {string}
      */
-    this.source = {
-      flag   : false,
-      content: ''
-    };
+    this.source = '';
 
     /**
      * ----------------------------------------------- 
      * Public Property (QuestionFormat.complete)
      * -----------------------------------------------
      * @desc This question's current completion status.
-     * @type {{
-     *   flag   : boolean,
-     *   content: string
-     * }}
+     * @type {string}
      */
-    this.complete = {
-      flag   : false,
-      content: ''
-    };
+    this.complete = '';
 
     /**
      * ----------------------------------------------- 
@@ -74,30 +61,24 @@
      * -----------------------------------------------
      * @desc This question's categories.
      * @type {{
-     *   flag: boolean,
      *   main: {
-     *     flag: boolean,
-     *     h3  : string,
-     *     p   : string
+     *     h3: string,
+     *     p : string
      *   },
      *   sub: {
-     *     flag: boolean,
-     *     h3  : string,
-     *     p   : string
+     *     h3: string,
+     *     p : string
      *   }
      * }}
      */
     this.category = {
-      flag: false,
       main: {
-        flag: false,
-        h3  : '',
-        p   : ''
+        h3: '',
+        p : ''
       },
       sub: {
-        flag: false,
-        h3  : '',
-        p   : ''
+        h3: '',
+        p : ''
       }
     };
 
@@ -107,13 +88,11 @@
      * -----------------------------------------------
      * @desc This question's solution.
      * @type {{
-     *   sError: boolean,
      *   code  : string,
      *   height: number
      * }}
      */
     this.solution = {
-      error : false,
       code  : '',
       height: 0
     };
@@ -123,36 +102,97 @@
      * Public Property (QuestionFormat.output)
      * -----------------------------------------------
      * @desc The solution's output for this question.
-     * @type {{
-     *   flag   : boolean,
-     *   content: string
-     * }}
+     * @type {string}
      */
-    this.output = {
-      flag   : false,
-      content: ''
-    };
+    this.output = '';
 
     /**
      * ----------------------------------------------- 
      * Public Property (QuestionFormat.links)
      * -----------------------------------------------
      * @desc This question's links.
-     * @type {{
-     *   flag   : boolean,
-     *   content: Array.<{
-     *     href: string,
-     *     name: string
-     *   }>
-     * }}
+     * @type {links}
      */
-    this.links = {
-      flag   : false,
-      content: []
-    };
+    this.links = [];
 
 
-    //
+    // Save the formats
+
+    // Format the id
+    if (app.config.questions.id && question.id) {
+      this.id = ( (question.id < 10) ?
+        '00' + question.id : (question.id < 100) ?
+          '0' + question.id : '' + question.id
+      );
+    }
+
+    // Format the source
+    if (app.sources.len && app.config.questions.source && question.source) {
+      this.source = app.sources.data[question.source].name;
+    }
+
+    // Format the completion status
+    if (app.configuration.questions.complete) {
+      this.complete = (question.complete) ? 'Yes' : 'No';
+    }
+
+    // Format the category
+    if (app.categories.len && app.config.questions.category) {
+
+      if (question.mainCat.length) {
+        // Set main category header
+        this.category.main.h3 = 'Main ' +
+        ( (question.mainCat.length > 1) ? 'Categories:' : 'Category:' );
+        // Set main category names
+        question.mainCat.forEach(function(/** string */ id, /** number */ i) {
+          this.category.main.p += (i === 0) ? '' : ', ';
+          this.category.main.p += app.categories.data[id].name;
+        }, this);
+      }
+
+      if (app.config.questions.subCat && question.subCat.length) {
+        // Set sub category header
+        this.category.sub.h3 = 'Sub ' +
+        ( (question.subCat.length > 1) ? 'Categories:' : 'Category:' );
+        // Set sub category names
+        question.subCat.forEach(function(/** string */ id, /** number */ i) {
+          this.category.sub.p += (i === 0) ? '' : ', ';
+          this.category.sub.p += app.categories.data[id].name;
+        }, this);
+      }
+    }
+
+    // Format the solution
+    if (question.solution) {
+
+      code = new PrettifiedList(solution);
+
+      this.solution.code = code.result;
+      this.solution.height = code.lineCount * app.elems.code.li.height;
+      this.solution.height += app.elems.code.ol.height;
+    }
+
+    // Format the output
+    if (app.config.questions.output && question.solution) {
+
+      this.output = question.solution();
+
+      if (typeof this.output !== 'string') {
+        this.output = String(this.output);
+      }
+    }
+
+    // Format the links
+    if (app.config.questions.links && question.links.length) {
+
+      question.links.forEach(function(/** string */ data) {
+        if (typeof data.name === 'string' &&
+            typeof data.href === 'string' &&
+            /(^http\:\/\/)|(^https\:\/\/)/.test(data.href)) {
+          this.links.push(data);
+        }
+      }, this);
+    }
 
 
     DEBUG && this.debug.group('init', 'end');
@@ -160,28 +200,3 @@
 
   // Ensure constructor is set to this class.
   QuestionFormat.prototype.constructor = QuestionFormat;
-
-  /**
-   * -----------------------------------------------------
-   * Public Method (QuestionFormat.prototype.)
-   * -----------------------------------------------------
-   * @desc .
-   * @param {}  - .
-   * @param {}  - .
-   * @param {}  - .
-   */
-  QuestionFormat.prototype.method = function() {
-
-    if (DEBUG) {
-      this.debug.start('method', args);
-      this.debug.args('method', arg, '?object');
-    }
-
-    /**
-     * @type {boolean}
-     * @private
-     */
-    var arg;
-
-    //
-  };
