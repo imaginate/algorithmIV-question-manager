a9656a6ff3b3b3c63ba639d6094f71e2b9462b1d
Adam Smith <adamsmith@youlum.com>
2015-03-19 03:33:22 -0700

    Added a JSDoc typedef for readability

--- test/app-core/skeleton.js
+++ test/app-core/skeleton.js
@@ -24,6 +24,7 @@
  * -----------------------------------------------------------------------------
  * Pre-Defined JSDoc Types
  * -----------------------------------------------------------------------------
+ * @typedef {*} val
  * @typedef {HTMLElement} elem
  * @typedef {Array<*>} vals
  * @typedef {Array<number>} nums
