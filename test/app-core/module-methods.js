ece5ee67e12f91114fbc672712df49a893d53df7
Adam Smith <adamsmith@youlum.com>
2015-03-23 06:01:58 -0700

    Added module methods capFirst & camelCase & deeper checks to checkType

--- test/app-core/module-methods.js
+++ test/app-core/module-methods.js
@@ -62,78 +62,133 @@
    * ---------------------------------------------------
    * Public Method (checkType)
    * ---------------------------------------------------
-   * @param {vals} vals - The value(s) to be evaluated.
-   * @param {(string|strings)} _types - The type(s) to evaluate the
+   * @param {vals} vals - An array of the value(s) to be evaluated.
+   *   Note that the values must be provided in an array.
+   * @param {(string|strings)} types - The type(s) to evaluate the
    *   value(s) against. The optional types are 'string', 'number',
-   *   'boolean', 'object', 'undefined', and 'array'. Use '|' as the
-   *   separator for multiple types (e.g. 'string|number'). Use '=' to
-   *   indicate the value is optional (e.g. 'array=' or 'string|number=').
-   *   Use '!' to indicate that null is not a possibility (e.g. '!string').
+   *   'boolean', 'object', 'undefined', 'array', 'strings', 'numbers',
+   *   'booleans', and 'objects'. Use '|' as the separator for multiple
+   *   types (e.g. 'strings|numbers'). Use '=' to indicate the value is
+   *   optional (e.g. 'array=' or 'string|number='). Use '!' to indicate
+   *   that null is not a possibility (e.g. '!string').
    * @return {boolean} The evaluation result.
    */
-  function checkType(vals, _types) {
+  function checkType(vals, types) {
 
+    // Debugging
+    var msg;
     if (DEBUG) {
-      debug.start('checkType', vals, _types);
-      debug.args('checkType', vals, 'array', _types, 'string|array');
+      debug.start('checkType', vals, types);
+      debug.args('checkType', vals, 'array', types, 'string|strings');
+      // Error message for type check
+      msg = 'Error: A given type was the wrong value. The incorrect ';
+      msg += 'value was \'$$\'. See the docs for acceptable values.';
     }
 
     /**
-     * @type {boolean}
+     * @type {RegExp}
      * @private
      */
-    var pass;
+    var arrays;
+    /**
+     * @type {RegExp}
+     * @private
+     */
+    var simple;
+    /**
+     * @type {RegExp}
+     * @private
+     */
+    var allTypes;
     /**
      * @type {*}
      * @private
      */
     var val;
 
-    if (typeof _types === 'string') {
-      _types = vals.map(function() {
-        return _types;
+    arrays = /^array$|^strings$|^numbers$|^booleans$|^objects$/;
+    simple = /^string$|^number$|^boolean$|^object$/;
+    allTypes = new RegExp('^undefined$|' + simple.source + '|' + arrays.source);
+
+    if (typeof types === 'string') {
+      types = vals.map(function() {
+        return types;
       });
     }
 
-    if (vals.length !== _types.length) {
+    if (vals.length !== types.length) {
       return false;
     }
 
-    pass = _types.every(function(/** string */ _type, /** number */ i) {
+    return types.every(function(/** string */ _type, /** number */ i) {
+      /**
+       * @type {strings}
+       * @private
+       */
+      var _types;
 
       val = vals[i];
-      _type = _type.toLowerCase().replace(/[^a-zA-Z\|\=\!]/g, '');
-      types = ( /\|/.test(_type) ) ? _type.split('|') : [ _type ];
+      _type = _type.toLowerCase().replace(/[^a-z\|\=\!]/g, '');
+      _types = ( /\|/.test(_type) ) ? _type.split('|') : [ _type ];
 
-      return types.some(function(/** string */ type) {
+      return _types.some(function(/** string */ type) {
+        /**
+         * @type {string}
+         * @private
+         */
+        var cleanType;
 
+        cleanType = type.replace(/\!|\=/g, '');
+
+        // Ensure a correct type was given
+        if ( !allTypes.test(cleanType) ) {
+          DEBUG && debug.fail('checkType', false, msg, type);
+          return false;
+        }
+
+        // Handle undefined val
         if (val === undefined) {
-          if (/\=/.test(type) || type === 'undefined') {
-            return true;
-          }
+          type = type.replace(/\!/g, '');
+          return /\=|^undefined$/.test(type);
         }
         else {
 
-          if (val === null && /\!/.test(type) === false) {
-            return true;
-          }
-          type = type.replace(/\!|\=/g, '');
-
-          if (type === 'array' && Array.isArray(val)) {
-            return true;
+          // Evaluate null
+          if (val === null) {
+            return !(/\!/.test(type));
           }
 
-          if (/(string)|(number)|(boolean)|(object)/.test(type) &&
-              typeof val === type) {
-            return true;
+          if (cleanType === 'undefined') {
+            return false;
+          }
+
+          // Evaluate array types
+          if ( arrays.test(cleanType) ) {
+
+            if ( !Array.isArray(val) ) {
+              return false;
+            }
+
+            if (cleanType === 'array') {
+              return true;
+            }
+
+            // Evaluate each value of the array
+            cleanType = cleanType.replace(/s$/, '');
+            return val.every(function(subVal) {
+              return (typeof subVal === cleanType);
+            });
+          }
+
+          // Evaluate string, number, boolean, and object types
+          if ( simple.test(cleanType) ) {
+            return (typeof val === cleanType);
           }
         }
 
         return false;
       });
     });
-
-    return pass;
   }
 
   /**
@@ -168,7 +223,17 @@
      */
     var name;
     /**
-     * @type {number}
+     * @type {num}
+     * @private
+     */
+    var id;
+    /**
+     * @type {num}
+     * @private
+     */
+    var len;
+    /**
+     * @type {num}
      * @private
      */
     var ii;
@@ -176,35 +241,95 @@
     keys = [];
     names = [];
 
-    ids.forEach(function(/** string */ id, /** number */ i) {
+    // Add the first key and its name to keys and names
+    i = ids.length - 1;
+    id = ids[i];
+    name = hMap[id].toLowerCase();
 
+    keys.push(id);
+    names.push(name);
+
+    // Add the remaining keys and their names in order
+    while (i--) {
+      id = ids[i];
       name = hMap[id].toLowerCase();
 
-      if (!i) {
-        keys.push(id);
-        names.push(name);
-      }
-      else {
+      // The sorting logic (pre-sorted keys get linear time)
+      len = names.length;
+      ii = 0;
+      while (true) {
 
-        ii = i - 1;
-        while (true) {
-
-          if (name >= names[ii]) {
-            ++ii;
-            keys.splice(ii, 0, id);
-            names.splice(ii, 0, name);
-            break;
-          }
-
-          if (ii === 0) {
-            keys.unshift(id);
-            names.unshift(name);
-            break;
-          }
-          --ii;
+        if (ii === len) {
+          keys.push(id);
+          names.push(name);
+          break;
         }
+
+        if (name <= names[ii]) {
+          keys.splice(ii, 0, id);
+          names.splice(ii, 0, name);
+          break;
+        }
+
+        ++ii;
       }
-    });
+    }
 
     return keys;
+  }
+
+  /**
+   * ---------------------------------------------------
+   * Public Method (capFirst)
+   * ---------------------------------------------------
+   * @desc A helper method that capitalizes the first letter of a string.
+   * @param {string} str - The original string.
+   * @return {string} The capitalized string.
+   */
+  function capFirst(str) {
+
+    if (DEBUG) {
+      debug.start('capFirst', str);
+      debug.args('capFirst', str, 'string');
+    }
+
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  /**
+   * ---------------------------------------------------
+   * Public Method (camelCase)
+   * ---------------------------------------------------
+   * @desc A helper method that converts a string with dashes to
+   *   camel case (e.g. 'example-case' to 'exampleCase').
+   * @param {string} str - The original string.
+   * @return {string} The camel case string.
+   */
+  function camelCase(str) {
+
+    if (DEBUG) {
+      debug.start('camelCase', str);
+      debug.args('camelCase', str, 'string');
+    }
+
+    /**
+     * @type {strings}
+     * @private
+     */
+    var arr;
+    /**
+     * @type {num}
+     * @private
+     */
+    var i;
+
+    arr = str.split('-');
+
+    // Capitalize the first letter in every word (except the first one)
+    i = arr.length;
+    while (--i) {
+      arr[i] = capFirst(arr[i]);
+    }
+
+    return arr.join('');
   }
