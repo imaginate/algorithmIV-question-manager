1bdf7352adf2fa0cfdd87d9ea051926985ee853f
Adam Smith <adamsmith@youlum.com>
2015-03-29 12:45:33 -0700

    Added Debug instance for DOM events

--- tests/pre-compiled-app/module-vars.js
+++ tests/pre-compiled-app/module-vars.js
@@ -5,7 +5,24 @@
    * @desc The Debug instance for the module's public methods.
    * @type {Debug}
    */
-  var debug = aIV.debug('module');
+  var debug = aIV.debug({
+    classTitle     : 'module',
+    turnOnDebuggers: 'args fail'
+  });
+
+  /**
+   * ----------------------------------------------- 
+   * Public Variable (events.debug)
+   * -----------------------------------------------
+   * @desc The Debug instance for the app's DOM events.
+   * @type {{ debug: Debug }}
+   */
+  var events = {
+    debug: aIV.debug({
+      classTitle     : 'Events',
+      turnOnDebuggers: 'args fail'
+    })
+  };
 
   /**
    * ----------------------------------------------- 
