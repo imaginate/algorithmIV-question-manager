da63a985daf74fc188f81ef604fdb40146c4671b
Adam Smith <adamsmith@youlum.com>
2015-04-03 04:47:28 -0700

    Added example data skeleton

---
+++ example/pre-compiled-settings/skeleton.js
@@ -0,0 +1,52 @@
+/* Algorithm IV Question Manager (v1.1.0) (learn@algorithmiv.com)
+ * Section: Example Settings & App Initialization
+ * Author: Adam Smith (adamsmith@youlum.com)
+ * Copyright (c) 2015 Adam A Smith (github.com/imaginate)
+ * The MIT License (algorithmiv.com/docs/license) */
+
+(function() {
+  "use strict";
+
+  /**
+   * -----------------------------------------------
+   * The Settings Object
+   * -----------------------------------------------
+   * @desc Contains all of the settings for running this example
+   *   of Algorithm IV's question manager. For more details see
+   *   [Algorithm IV's online documentation]{@link algorithmiv.com/docs/start}.
+   * @type {Object}
+   */
+  settings = {};
+
+
+/* -----------------------------------------------------------------------------
+ * | The Configuration                                                         |
+ * v ------------------------------------------------------------------------- v
+                             example/pre-compiled-settings/configuration.js */
+
+
+/* -----------------------------------------------------------------------------
+ * | The Sources                                                               |
+ * v ------------------------------------------------------------------------- v
+                                   example/pre-compiled-settings/sources.js */
+
+
+/* -----------------------------------------------------------------------------
+ * | The Categories                                                            |
+ * v ------------------------------------------------------------------------- v
+                                example/pre-compiled-settings/categories.js */
+
+
+/* -----------------------------------------------------------------------------
+ * | The Questions                                                             |
+ * v ------------------------------------------------------------------------- v
+                                 example/pre-compiled-settings/questions.js */
+
+
+/* -----------------------------------------------------------------------------
+ * | The Initiation                                                            |
+ * v ------------------------------------------------------------------------- v
+                                      example/pre-compiled-settings/init.js */
+
+
+})();
