e11ff3118f2b5dbaac005f52ec1d8d64f7e6c050
Adam Smith <adamsmith@youlum.com>
2015-04-03 04:48:37 -0700

    Added example sources

---
+++ example/pre-compiled-settings/sources.js
@@ -0,0 +1,15 @@
+  /**
+   * -----------------------------------------------
+   * The Sources
+   * -----------------------------------------------
+   * @desc An object property of settings that contains all of the
+   *   question manager's source ids and names. For more details see the
+   *   [online documentation for sources]{@link algorithmiv.com/docs/sources}.
+   * @type {Object<string, string>}
+   */
+  settings.sources = {
+    'am': 'Amazon',
+    'bl': 'Bloomberg',
+    'fb': 'Facebook',
+    'go': 'Google'
+  };
