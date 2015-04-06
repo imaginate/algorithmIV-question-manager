64cc2a0b8c4d1a7696907a2f043653745b9ae5fd
Adam Smith <adamsmith@youlum.com>
2015-04-06 14:36:30 -0700

    Added settings.resources to example

---
+++ example/pre-compiled-settings/resources.js
@@ -0,0 +1,11 @@
+  /**
+   * -----------------------------------------------
+   * The Resources
+   * -----------------------------------------------
+   * @desc An object property of settings that contains all of the JSON
+   *   resources that are made available for use in the questions via the
+   *   public method aIV.app.getResource(resourceName). For more details see the
+   *   [online documentation for resources]{@link algorithmiv.com/docs/resources}.
+   * @type {(string|Array<string>)}
+   */
+  settings.resources = 'words';
