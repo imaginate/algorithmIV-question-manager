c1a7325c48c4ce87f02f38b4fcd2ca500df103a7
Adam Smith <adamsmith@youlum.com>
2015-05-07 04:15:20 -0700

    Updated the App Module Vars

--- tests/pre-compiled-app/module-vars.js
+++ tests/pre-compiled-app/module-vars.js
@@ -1,14 +1,22 @@
   // The debugging vars
   var debug = aIV.debug('module');
-  var polyfill = { debug: aIV.debug('polyfill') };
   var debugArgs, debugMsg, debugCheck;
+
+  /**
+   * -----------------------------------------------------
+   * Public Variable (hasAppBeenInitialized)
+   * -----------------------------------------------------
+   * @desc Indicates whether the app has been initialized.
+   * @type {boolean}
+   */
+  var hasAppBeenInitialized = false;
 
   /**
    * ----------------------------------------------- 
    * Public Variable (resources)
    * -----------------------------------------------
    * @desc The resources for the app.
-   * @type {Object}
+   * @type {!Object}
    */
   var resources = {};
 
@@ -17,96 +25,6 @@
    * Public Variable (app)
    * -----------------------------------------------
    * @desc The app instance.
-   * @type {App}
+   * @type {!App}
    */
   var app;
-
-  /**
-   * ----------------------------------------------- 
-   * Public Variable (regexps)
-   * -----------------------------------------------
-   * @desc Regular expressions that are used multiple times
-   *   throughout the app.
-   * @type {Object<string, Object>}
-   */
-  var regexps = {};
-
-  /**
-   * ----------------------------------------------- 
-   * Public Variable (regexps.types)
-   * -----------------------------------------------
-   * @desc Regular expressions that contain types.
-   * @type {Object<string, RegExp>}
-   */
-  regexps.types = {};
-
-  /**
-   * ----------------------------------------------- 
-   * Public Variable (regexps.types.all)
-   * -----------------------------------------------
-   * @desc All the types available.
-   * @type {RegExp}
-   */
-  regexps.types.all = (function() {
-    /** @type {string} */
-    var types;
-
-    types = '' +
-    '^string$|^number$|^boolean$|^object$|^array$|^function$|^elem$|'          +
-    '^undefined$|^strings$|^numbers$|^booleans$|^objects$|^arrays$|^elems$|'   +
-    '^functions$|^stringmap$|^numbermap$|^booleanmap$|^objectmap$|^arraymap$|' +
-    '^functionmap$|^elemmap$';
-
-    return new RegExp(types);
-  })();
-
-  /**
-   * ----------------------------------------------- 
-   * Public Variable (regexps.types.basic)
-   * -----------------------------------------------
-   * @desc The basic types available.
-   * @type {RegExp}
-   */
-  regexps.types.basic = (function() {
-    /** @type {string} */
-    var types;
-
-    types = '^string$|^number$|^boolean$|^object$|' +
-            '^function$|^elem$|^undefined$';
-
-    return new RegExp(types);
-  })();
-
-  /**
-   * ----------------------------------------------- 
-   * Public Variable (regexps.types.arrays)
-   * -----------------------------------------------
-   * @desc The array types available.
-   * @type {RegExp}
-   */
-  regexps.types.arrays = (function() {
-    /** @type {string} */
-    var types;
-
-    types = '^array$|^strings$|^numbers$|^booleans$|' +
-            '^objects$|^arrays$|^elems$|^functions$';
-
-    return new RegExp(types);
-  })();
-
-  /**
-   * ----------------------------------------------- 
-   * Public Variable (regexps.types.maps)
-   * -----------------------------------------------
-   * @desc The hash map types available.
-   * @type {RegExp}
-   */
-  regexps.types.maps = (function() {
-    /** @type {string} */
-    var types;
-
-    types = '^stringmap$|^numbermap$|^booleanmap$|^objectmap$|' +
-            '^arraymap$|^functionmap$|^elemmap$';
-
-    return new RegExp(types);
-  })();
