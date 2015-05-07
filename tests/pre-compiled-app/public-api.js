cd75f8524378913f81d13228d0331124c879f8e4
Adam Smith <adamsmith@youlum.com>
2015-05-07 15:29:37 -0700

    Added non-nullable JSDoc to the App Public API

--- tests/pre-compiled-app/public-api.js
+++ tests/pre-compiled-app/public-api.js
@@ -13,11 +13,11 @@
    * Global Method (aIV.app)
    * ---------------------------------------------------
    * @desc Initializes the aIV question management app.
-   * @param {Object} settings - The app's settings.
+   * @param {!Object} settings - The app's settings.
    * @param {objectMap=} settings.config - The app's configuration.
    * @param {stringMap=} settings.sources - The app's sources.
    * @param {(objectMap|stringMap)=} settings.categories - The app's categories.
-   * @param {objects} settings.questions - The app's questions.
+   * @param {!objects} settings.questions - The app's questions.
    * @param {(string|strings)=} settings.resources - The app's resources.
    * @global
    */
