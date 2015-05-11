65e75b48ca4f621b6472a9f03f42f4f5bb8a5a1d
Adam Smith <adamsmith@youlum.com>
2015-05-11 03:20:53 -0700

    For LinksConfig - cleaned up & added checkArgs & debug.end

--- tests/pre-compiled-app/classes/config/links-config.js
+++ tests/pre-compiled-app/classes/config/links-config.js
@@ -4,17 +4,20 @@
    * -----------------------------------------------------
    * @desc The configuration settings for whether to show search links for
    *   portions of each question.
-   * @param {Object} config - The user's config settings for search link
-   *   formatting.
+   * @param {!Object<string, boolean>} config - The user's config settings
+   *   for search link formatting.
    * @constructor
    */
   var LinksConfig = function(config) {
 
+    var thisDebug;
+
     this.debug = aIV.debug('LinksConfig');
+    thisDebug = this.debug;
 
     this.debug.start('init', config);
 
-    this.debug.args('init', config, 'object');
+    checkArgs(config, '!object');
 
     ////////////////////////////////////////////////////////////////////////////
     // Define The Protected Properties
@@ -54,19 +57,9 @@
     // Setup The Protected Properties
     ////////////////////////////////////////////////////////////////////////////
 
-    id       = true;
-    source   = false;
-    category = true;
-
-    if (config.hasOwnProperty('id') && config.id === false) {
-      id = false;
-    }
-    if (config.hasOwnProperty('source') && config.source === true) {
-      source = true;
-    }
-    if (config.hasOwnProperty('category') && config.category === false) {
-      category = false;
-    }
+    id       = !(config.id       === false);
+    source   =  (config.source   === true );
+    category = !(config.category === false);
 
     ////////////////////////////////////////////////////////////////////////////
     // Define & Setup The Public Methods
@@ -78,36 +71,26 @@
      * -----------------------------------------------
      * @desc Gets a protected property's value from LinksConfig.
      * @param {string} prop - The name of the property to get.
-     * @return {boolean}
+     * @return {boolean} The property's value.
      */
     this.get = function(prop) {
 
-      this.debug.start('get', prop);
-      this.debug.args('get', prop, 'string');
-
-      /** @type {Object<string, boolean>} */
+      /** @type {!Object<string, boolean>} */
       var props = {
+        debug   : thisDebug,
         id      : id,
         source  : source,
         category: category
       };
 
-      debugCheck = props.hasOwnProperty(prop);
-      debugMsg = 'Error: The given property does not exist. property= $$';
-      this.debug.fail('get', debugCheck, debugMsg, prop);
-
-      return props[ prop ];
+      return getter.call(props, prop);
     };
-
-    // Freeze all of the methods
-    freezeObj(this.get);
 
     ////////////////////////////////////////////////////////////////////////////
     // End Of The Class Setup
     ////////////////////////////////////////////////////////////////////////////
 
-    // Freeze this class instance
-    freezeObj(this);
+    this.debug.end('init');
   };
 
 ////////////////////////////////////////////////////////////////////////////////
