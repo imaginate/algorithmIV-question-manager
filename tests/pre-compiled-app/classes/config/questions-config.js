f57bba763e922b79a6f2dca67caa608cd5505d7a
Adam Smith <adamsmith@youlum.com>
2015-05-11 03:21:17 -0700

    For QuestionsConfig - cleaned up & added checkArgs & debug.end

--- tests/pre-compiled-app/classes/config/questions-config.js
+++ tests/pre-compiled-app/classes/config/questions-config.js
@@ -3,16 +3,19 @@
    * Public Class (QuestionsConfig)
    * -----------------------------------------------------
    * @desc The configuration settings for formatting questions in this app.
-   * @param {Object} config - The user's question format config settings.
+   * @param {!Object} config - The user's question format config settings.
    * @constructor
    */
   var QuestionsConfig = function(config) {
 
+    var thisDebug;
+
     this.debug = aIV.debug('QuestionsConfig');
+    thisDebug = this.debug;
 
     this.debug.start('init', config);
 
-    this.debug.args('init', config, 'object');
+    checkArgs(config, '!object');
 
     ////////////////////////////////////////////////////////////////////////////
     // Define The Protected Properties
@@ -112,44 +115,15 @@
     // Setup The Protected Properties
     ////////////////////////////////////////////////////////////////////////////
 
-    id       = true;
-    complete = true;
-    source   = true;
-    category = true;
-    subCat   = true;
-    links    = true;
-    problem  = true;
-    descr    = false;
-    output   = true;
-
-    if (config.hasOwnProperty('id') && config.id === false) {
-      id = false;
-    }
-    if (config.hasOwnProperty('complete') && config.complete === false) {
-      complete = false;
-    }
-    if (config.hasOwnProperty('source') && config.source === false) {
-      source = false;
-    }
-    if (config.hasOwnProperty('category') && config.category === false) {
-      category = false;
-    }
-    if (config.hasOwnProperty('subCat') && config.subCat === false) {
-      subCat = false;
-    }
-    if (config.hasOwnProperty('links') && config.links === false) {
-      links = false;
-    }
-    if (config.hasOwnProperty('problem') && config.problem === false) {
-      problem = false;
-    }
-    if (config.hasOwnProperty('descr') && config.descr === true) {
-      descr = true;
-    }
-    if (config.hasOwnProperty('output') && config.output === false) {
-      output = false;
-    }
-
+    id       = !(config.id       === false);
+    complete = !(config.complete === false);
+    source   = !(config.source   === false);
+    category = !(config.category === false);
+    subCat   = !(config.subCat   === false);
+    links    = !(config.links    === false);
+    problem  = !(config.problem  === false);
+    descr    =  (config.descr    === true );
+    output   = !(config.output   === false);
 
     ////////////////////////////////////////////////////////////////////////////
     // Define & Setup The Public Methods
@@ -161,15 +135,13 @@
      * -----------------------------------------------
      * @desc Gets a protected property's value from QuestionsConfig.
      * @param {string} prop - The name of the property to get.
-     * @return {boolean}
+     * @return {boolean} The property's value.
      */
     this.get = function(prop) {
 
-      this.debug.start('get', prop);
-      this.debug.args('get', prop, 'string');
-
-      /** @type {Object<string, boolean>} */
+      /** @type {!Object<string, boolean>} */
       var props = {
+        debug   : thisDebug,
         id      : id,
         complete: complete,
         source  : source,
@@ -181,22 +153,14 @@
         output  : output
       };
 
-      debugCheck = props.hasOwnProperty(prop);
-      debugMsg = 'Error: The given property does not exist. property= $$';
-      this.debug.fail('get', debugCheck, debugMsg, prop);
-
-      return props[ prop ];
+      return getter.call(props, prop);
     };
-
-    // Freeze all of the methods
-    freezeObj(this.get);
 
     ////////////////////////////////////////////////////////////////////////////
     // End Of The Class Setup
     ////////////////////////////////////////////////////////////////////////////
 
-    // Freeze this class instance
-    freezeObj(this);
+    this.debug.end('init');
   };
 
 ////////////////////////////////////////////////////////////////////////////////
