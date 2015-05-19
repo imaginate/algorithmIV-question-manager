c3b9efbfeff4118f53a06cf97587e863a5ffe60c
Adam Smith <adamsmith@youlum.com>
2015-05-18 17:34:47 -0700

    For Tests replaced all & added new helpers

--- tests/pre-compiled-tests/module-methods.js
+++ tests/pre-compiled-tests/module-methods.js
@@ -1,23 +1,131 @@
   /**
    * ---------------------------------------------
-   * Public Method (getID)
+   * Public Method (getElemById)
    * ---------------------------------------------
-   * @desc A shortcut for getElementById.
-   * @param {string} title - The name of the id of the element to select.
-   * @return {HTMLElement} A reference to element with the given id.
+   * @desc A shortcut for the native DOM method - document.getElementById.
+   * @param {string} id - The id of the element to select.
+   * @return {!HTMLElement} The DOM element with the given id.
    */
-  function getID(title) {
-    return document.getElementById(title);
-  }
+  var getElemById = aIV.utils.getElemById;
+
+  /**
+   * ---------------------------------------------------
+   * Public Method (getElemByClass)
+   * ---------------------------------------------------
+   * @desc A shortcut for the native DOM method -
+   *   [DOM Node].getElementsByClassName[ [index] ].
+   * @param {string} classname - The class name of the element to select.
+   * @param {number=} index - The index of the array of found elements to
+   *   select. The default is 0.
+   * @param {!(Document|Element)=} root - Limit the selections to this element's
+   *   children. The default is document or the element set with
+   *   aIV.utils.set({ getElemByClassRoot: [DOM Node] }).
+   * @return {!HTMLElement} The selected DOM element.
+   */
+  var getElemByClass = aIV.utils.getElemByClass;
+
+  /**
+   * ---------------------------------------------------
+   * Public Method (getElemsByClass)
+   * ---------------------------------------------------
+   * @desc A shortcut for the native DOM method -
+   *   [DOM Node].getElementsByClassName.
+   * @param {string} classname - The class name of the elements to select.
+   * @param {!(Document|Element)=} root - Limit the selections to this element's
+   *   children. The default is document or the element set with
+   *   aIV.utils.set({ getElemsByClassRoot: [DOM Node] }).
+   * @return {!Array<HTMLElement>} The selected DOM elements.
+   */
+  var getElemsByClass = aIV.utils.getElemsByClass;
+
+  /**
+   * ---------------------------------------------------
+   * Public Method (freezeObj)
+   * ---------------------------------------------------
+   * @desc A shortcut for the Object.freeze method with a deep freeze option.
+   * @param {!(Object|function)} obj - The object to freeze.
+   * @param {boolean=} deep - Deep freeze the object. The default is false.
+   * @return {!(Object|function)} The frozen object.
+   */
+  var freezeObj = aIV.utils.freezeObj;
+
+  /**
+   * ---------------------------------------------------
+   * Public Method (hasOwnProp)
+   * ---------------------------------------------------
+   * @desc A shortcut for the Object.prototype.hasOwnProperty method.
+   * @param {!object|function} obj - The object to check.
+   * @param {string} prop - The property to check.
+   * @return {boolean} The result of the check.
+   */
+  var hasOwnProp = aIV.utils.hasOwnProp;
+
+  /**
+   * ---------------------------------------------------
+   * Public Method (checkType)
+   * ---------------------------------------------------
+   * @desc Checks a value's data type against the given optional types.
+   * @param {*} val - The value to be evaluated.
+   * @param {string} type - A string of the data types to evaluate the value
+   *   against. For a complete list of acceptable strings
+   *   [see aIV.utils.checkType]{@link https://github.com/imaginate/algorithmIV-javascript-shortcuts/blob/master/src/pre-compiled-parts/methods/checkType.js}.
+   * @param {boolean=} noTypeValCheck - If true skips the data type string checks.
+   *   The default is false. Use to avoid duplicating checks.
+   * @return {boolean} The evaluation result.
+   */
+  var checkType = aIV.utils.checkType;
+
+  /**
+   * ---------------------------------------------------
+   * Public Method (isValidTypeString)
+   * ---------------------------------------------------
+   * @desc Evaluates whether a string is a valid data type string.
+   * @param {string} type - The string to evaluate.
+   * @return {boolean} The evaluation result.
+   */
+  var isValidTypeString = aIV.utils.isValidTypeString;
 
   /**
    * ---------------------------------------------
-   * Public Method (getClass)
+   * Public Method (copyObj)
    * ---------------------------------------------
-   * @desc A shortcut for getElementsByClassName.
-   * @param {string} title - The name of the class to select.
-   * @return {Array<HTMLElement>} The elements with the class name.
+   * @desc A shortcut that creates a new object with the same properties and
+   *   values as the given object.
+   * @param {!Object} oldObj - The object to copy from.
+   * @return {!Object} The new copied object.
    */
-  function getClass(title) {
-    return getID('aIV-tests').getElementsByClassName(title);
-  }
+  var copyObj = (function setup_copyObj() {
+
+    /** @type {string} */
+    var errorMsg;
+    /** @type {function} */
+    var throwTypeError;
+
+    errorMsg = 'An aIV.tests copyObj call received an invalid oldObj param.';
+    throwTypeError = function() {
+      throw new TypeError(errorMsg);
+    };
+
+    return function copyObj(oldObj) {
+
+      /** @type {!Object} */
+      var newObj;
+      /** @type {string} */
+      var prop;
+
+      if ( !checkType(oldObj, '!object|function') ) {
+        throwTypeError();
+        oldObj = {};
+      }
+
+      newObj = {};
+
+      for (prop in oldObj) {
+        if ( hasOwnProp(oldObj, prop) ) {
+          newObj[ prop ] = oldObj[ prop ];
+        }
+      }
+
+      return newObj;
+    };
+  })();
